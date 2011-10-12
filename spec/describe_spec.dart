63079e378d000cbc05635a1ca67d31e9135cc282
remi <remi@remitaylor.com>
2011-10-12 04:22:13 -0700

    Added some pending TODO specs

--- spec/describe_spec.dart
+++ spec/describe_spec.dart
@@ -1,8 +1,25 @@
+class DescribeSpec_NoExamples extends Spec {}
+class DescribeSpec_ManyExamples extends Spec {}
+class DescribeSpec_ManyDescribesNoExamples extends Spec {}
+class DescribeSpec_ManyDescribesManyExamples extends Spec {}
+
 class DescribeSpec extends SpecMap {
   spec() {
+
+    var noExamples                = new DescribeSpec_NoExamples();
+    var manyExamples              = new DescribeSpec_ManyExamples();
+    var manyDescribesNoExamples   = new DescribeSpec_ManyDescribesNoExamples();
+    var manyDescribesManyExamples = new DescribeSpec_ManyDescribesManyExamples();
+
     describe("Describe", {
 
+      "can have no examples": null,
 
+      "can have many examples": null,
+
+      "can have many sub-describes with no examples": null,
+
+      "can have many sub-describes with examples": null
 
     });
   }
--- spec/spec_spec.dart
+++ spec/spec_spec.dart
@@ -1,16 +1,18 @@
-class SpecSpec_NoDescribes   extends Spec {}
+class SpecSpec_NoDescribes extends Spec {}
 class SpecSpec_ManyDescribes extends Spec {
   spec() {
     describe("Foo");
     describe("Bar");
   }
 }
+class SpecSpec_ManyDescribesWithExamples extends Spec {}
 
 class SpecSpec extends SpecMap {
   spec() {
 
-    var noDescribes   = new SpecSpec_NoDescribes();
-    var manyDescribes = new SpecSpec_ManyDescribes();
+    var noDescribes               = new SpecSpec_NoDescribes();
+    var manyDescribes             = new SpecSpec_ManyDescribes();
+    var manyDescribesWithExamples = new SpecSpec_ManyDescribesWithExamples();
 
     describe("Spec", {
 
@@ -22,7 +24,9 @@ class SpecSpec extends SpecMap {
         Expect.equals(2, manyDescribes.describes.length);
         Expect.equals("Foo", manyDescribes.describes[0].subject);
         Expect.equals("Bar", manyDescribes.describes[1].subject);
-      }
+      },
+
+      "can have many describes with examples": null
 
     });
   }
